c25c0010d0d0c2923635aca7dd6a0633ebbc6173
LiuJuncheng <58631824+Liujc1965@users.noreply.github.com>
2024-03-08 10:46:52 +0800

    新项目

---
+++ HelloWorld22/entry/src/main/ets/pages/ImagePage.js
@@ -0,0 +1,9 @@
+"use strict";
+struct ImagePage extends   {
+    constructor() { }
+    build() {
+            .width('100%')
+            .height('100%');
+    }
+}
+//# sourceMappingURL=ImagePage.js.map
